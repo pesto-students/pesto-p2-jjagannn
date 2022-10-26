18e4dcdddba4ee83f4cc76e5e525066628585fd5
Jagannathan J <jjagannn@gmail.com>
2022-10-26 11:09:16 +0530

    Updated line breaks after each section

--- Week-8/Assignment8_3.js
+++ Week-8/Assignment8_3.js
@@ -23,25 +23,33 @@
  var levelOrder = function(root) {
     
     let queue = [], traversal = [];
+
     if(root){
         queue.push(root);
         // traversal.push([root.val]);
     }
+
     while(queue.length !== 0){
+
         let currQlength = queue.length;
         let currLevel = [];
         
         for (let i=0; i< currQlength; ++i){
             let node = queue.shift();
+
             if(node.left){
                 queue.push(node.left);
             }
+
             if(node.right){
                 queue.push(node.right);
             }
+
             currLevel.push(node.val)
         }
+
         traversal.push(currLevel)
     }
+    
     return traversal;
 };
